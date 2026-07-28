0198bfe8d123ab9f4f265409b96447b98ff536b9
Nyior <nyior.clement@aun.edu.ng>
2026-07-28 09:29:33 +0100

    Get rid of old project files

--- coding_tools_advisor/shared/utils.py
+++
@@ -1,8 +0,0 @@
-import itertools
-
-
-def batched(iterable, batch_size: int):
-    """Yield successive batch_size-sized chunks from an iterable."""
-    it = iter(iterable)
-    while batch := list(itertools.islice(it, batch_size)):
-        yield batch
